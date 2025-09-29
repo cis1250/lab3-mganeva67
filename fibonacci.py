2313667a94af22f54d4c27a844160b1352e5a8e7
mganeva67 <mganeva@uoguelph.ca>
2025-09-29 09:54:50 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -3,5 +3,21 @@
 # Fibonacci Sequence Exercise
 # TODO: (Read detailed instructions in the Readme file)
 # Prompt the user for the number of terms.
+while True:
+  try:
+    num_terms = input("Enter the number of Fibonacci terms you want: ")
 # Validate that the input is a positive integer.
+N = int(num_tearms)
+if N <= 0:
+  print("Please enter a positive integer")
 # Use a for loop to print the Fibonacci sequence up to that many terms.
+continue
+break
+except ValueError:
+print("Please enter a positive interger")
+a, b = 0, 1
+if N > 0:
+  for i in range(N):
+    print(a, end=" ")
+print()
+    
